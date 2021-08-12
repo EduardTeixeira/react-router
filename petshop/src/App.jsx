4a96381bf470b2386b0f8a3a5d2d6477468cedbd
unknown <eduard.teixeira@etec.sp.gov.br>
2021-08-12 10:47:15 -0300

    router

--- petshop/src/App.jsx
+++ petshop/src/App.jsx
@@ -1,12 +1,33 @@
 import React from 'react'
 import './assets/css/base/base.css'
+import Home from './paginas/Home'
+import Sobre from './paginas/Sobre'
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 
 function App() {
-  return (
-  <>
 
-  </>
+  // <Switch></Switch> - evita que o ReactRouter fique procurando pelo mesmo path
+
+  return (
+
+    <Router>
+
+      <Switch>
+
+        <Route exact path='/'>
+          <Home />
+        </Route>
+
+        <Route path='/sobre'>
+          <Sobre />
+        </Route>
+
+      </Switch>
+
+    </Router>
+
   )
+
 }
 
 export default App
