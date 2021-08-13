9b666a48925042e620a01b6c5ab810f1f3274472
unknown <eduard.teixeira@etec.sp.gov.br>
2021-08-13 16:30:10 -0300

    exibindo post e categoria

--- petshop/src/paginas/Home.jsx
+++ petshop/src/paginas/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import ListaCategorias from '../components/ListaCategorias'
 
 import ListaPost from '../components/ListaPost'
 
@@ -11,6 +12,8 @@ const Home = () => {
       <div className="container">
         <h2 className="titulo-pagina">Pet notícias</h2>
       </div>
+
+      <ListaCategorias />
 
       <ListaPost url={'/posts'} />
 
